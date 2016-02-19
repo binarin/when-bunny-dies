98922deb9640a276d68061d855139948d0a8e258
Alexey Lebedeff <alebedev@mirantis.com>
2016-02-19 16:56:42 +0300

    Use ets for interactive focusing on objects

--- user_default.erl
+++ user_default.erl
@@ -4,6 +4,20 @@
 -include_lib("rabbitmq_server/include/rabbit.hrl").
 -include_lib("rabbitmq_server/include/rabbit_framing.hrl").
 
+ensure_ets() ->
+    case catch ets:new(when_bunny_dies, [public, named_table]) of
+        {'EXIT', _} ->
+            %% Was created earlier
+            ok;
+        _ ->
+            ets:give_away(when_bunny_dies, erlang:whereis(init), undefined)
+    end,
+    ok.
+
+focus(Kind, Data) ->
+    ensure_ets(),
+    ets:insert(when_bunny_dies, {Kind, Data}).
+
 queue(Name) ->
     {ok, Q} = rabbit_misc:dirty_read({rabbit_queue, {resource, <<"/">>, queue, atom_to_binary(Name, utf8)}}),
     Q.
@@ -12,3 +26,9 @@ qpid(Name) when is_atom(Name) ->
     qpid(queue(Name));
 qpid(#amqqueue{pid = Pid}) ->
     Pid.
+
+peer_port_conn(Port) ->
+    Conn = hd([ Pid || Pid <- rabbit_networking:connections(),
+                       element(2, hd(rabbit_reader:info(Pid, [peer_port]))) =:= Port]),
+    focus(connection, Conn),
+    Conn.
