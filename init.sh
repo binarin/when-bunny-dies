6ace938b16b987ab5ff7b334beaa05f74a3c9ef4
Alexey Lebedeff <alebedev@mirantis.com>
2016-02-19 15:09:52 +0300

    Change default directory
    /root/ can be unreadable by rabbit

--- init.sh
+++ init.sh
@@ -2,8 +2,10 @@
 set -o errexit
 set -o pipefail
 
-if [[ ! -d $HOME/.when-bunny-dies ]]; then
+TARGET=/tmp/
+
+if [[ ! -d $TARGET/.when-bunny-dies ]]; then
     git clone https://github.com/binarin/when-bunny-dies "$HOME/.when-bunny-dies"
 fi
 
-make -C $HOME/.when-bunny-dies
+make -C $TARGET/.when-bunny-dies
